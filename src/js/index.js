e004e9bd1d3026666db81cb9572709bcaed3bf20
Abel Segui <Abelsegui@hotmail.com>
2020-06-16 01:55:28 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Timer } from "./component/timer.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Timer />, document.querySelector("#app"));
